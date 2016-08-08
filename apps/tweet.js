43d3f318615d66ba17ecfa0da0b9e8d6de7cd111
tsunosekai <aokibazooka@gmail.com>
2016-08-08 22:39:41 +0900

    Update tweet.js

--- apps/tweet.js
+++ apps/tweet.js
@@ -1,10 +1,10 @@
 var Twit = require('twit')
 
 var T = new Twit({
-  consumer_key:         'BheodSH4tWds1TWhULLYCiNMe',
-  consumer_secret:      'ycxUij1mSERvNv9sMrABDgSm4VDtjjlai6WhzCjNgonzetfIvB',
-  access_token:         '3253194403-JAjsE8LjWOR2aoOCaGcJCWCCUoDT39C51ZtnHme',
-  access_token_secret:  'pkp2em6qhX2kPkUvj7LG8YELQKoyV6hiOJDwT3duKbn0n',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
 
